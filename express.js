10bb98d84fb268d1cb401bcf163960421570bf5f
Shi Hung <c.shihhung@gmail.com>
2017-05-25 18:05:03 +0000

    Completed lesson five

--- express.js
+++ express.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var path = require('path');
 var app = express();
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static(process.argv[3]));
+app.use(require('stylus').middleware(process.argv[3]));
+/*app.use(bodyparser.urlencoded({extended: false}));
 app.post('/form', function(req, res){
    res.end(req.body.str.split('').reverse().join(''));
-});
+});*/
 app.listen(process.argv[2]);
