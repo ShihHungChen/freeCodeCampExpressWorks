90b43577cce7c5101508f41664a86f2ffd0e1883
Shi Hung <c.shihhung@gmail.com>
2017-05-26 17:20:45 +0000

    Completed lesson eight

--- express.js
+++ express.js
@@ -1,9 +1,20 @@
 console.log(process.argv);
+var fs = require('fs');
 var express = require('express');
 var app = express();
-app.get('/search', function(req, res){
-    var query = req.query;
-    delete query.__proto__;
-    res.send(JSON.stringify(query));
+app.set('json spaces', 0);
+app.get('/books', function(req, res){
+    fs.readFile(process.argv[3], function(err, data){
+        if(err){
+            return res.send(500);
+        }
+        try{
+            var file_object = JSON.parse(data);
+        } catch(err){
+            res.send(500);
+        }
+        res.json(file_object);
+        res.json(file_object);
+    });
 })
 app.listen(process.argv[2]);
